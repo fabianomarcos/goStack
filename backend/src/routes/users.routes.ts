bf7e5898b014057a844fc0f201443231d937049b
Fabiano Marcos <fabiano@vexter.com.br>
2020-08-30 00:17:50 -0300

    upload images

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,7 +1,13 @@
-import { Router } from 'express';
+import { Router, response, request } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/updateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -21,5 +27,26 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete (await user).password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  })
 
 export default usersRouter;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
-
+import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
 
 app.use(routes);
-
+app.use('/files', express.static(uploadConfig.directory));
 app.listen(3333, () => {
   console.log('✔ Server started on port 3333.');
 });
